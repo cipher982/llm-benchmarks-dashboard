1de11d90e98affad4498c612119a659bf90c2a9a
drose <david@drose.io>
2023-10-24 19:19:47 -0400

    add more data to the table

--- backend/models/BenchmarkMetrics.js
+++ backend/models/BenchmarkMetrics.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const BenchmarkMetricsSchema = new mongoose.Schema({
   model_name: String,
   tokens_per_second: [Number],
+  gpu_mem_usage: [Number],
+  output_tokens: [Number],
+  quantization_bits: String,
+  torch_dtype: String,
 });
 
 const BenchmarkMetrics = mongoose.model("benchmark_metrics", BenchmarkMetricsSchema);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,18 @@ function App() {
       });
   }, []);
 
+  // Function to calculate the mean of an array
+  const calculateMean = (arr) => {
+    if (!Array.isArray(arr) || arr.length === 0) return null;
+    const sum = arr.reduce((a, b) => a + b, 0);
+    return sum / arr.length;
+  };
+
+  // Function to convert bytes to GB
+  const bytesToGB = (bytes) => {
+    return bytes / (1024 * 1024 * 1024);
+  };
+
   return (
     <div>
       {loading ? "Loading..." : null}
@@ -28,17 +40,21 @@ function App() {
         <thead>
           <tr>
             <th>Model Name</th>
-            <th>Tokens Per Second</th>
+            <th>Tokens/Second</th>
+            <th>GPU Memory</th>
+            <th>Quantization</th>
+            <th>Torch DType</th>
           </tr>
         </thead>
         <tbody>
           {benchmarks.map((benchmark, index) => (
             <tr key={index}>
               <td>{benchmark.model_name}</td>
-              <td>{Array.isArray(benchmark.tokens_per_second) 
-                    ? benchmark.tokens_per_second.join(", ") 
-                    : benchmark.tokens_per_second}
-              </td>
+              <td>{calculateMean(benchmark.tokens_per_second).toFixed(2)}</td>
+              <td>{bytesToGB(calculateMean(benchmark.gpu_mem_usage)).toFixed(2) + " GB" || "None"}</td>
+              <td>{benchmark.quantization_bits || "None"}</td>
+              <td>{benchmark.torch_dtype}</td>
+              
             </tr>
           ))}
         </tbody>
