3b571ae439f36a586179b43ea3647d8739dff575
drose <david@drose.io>
2023-10-27 17:31:49 -0400

    update mongo connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/benchmarks")
+    fetch("http://llm-bench-back.fly.dev/api/benchmarks")
       .then((res) => res.json())
       .then((data) => {
         console.log("Fetched data:", data);
