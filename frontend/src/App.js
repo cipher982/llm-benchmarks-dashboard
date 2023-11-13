90f4cf87470b00f6e2c12dbd0f2aff68c86a1d51
drose <david@drose.io>
2023-11-13 18:04:19 -0500

    added dedupe averaging for multiple model runs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -115,7 +115,6 @@ const App = () => {
 
   useEffect(() => {
     fetch("https://llm-bench-back.fly.dev/api/benchmarks")
-      // fetch("http://localhost:5000/api/benchmarks")
       .then((res) => res.json())
       .then((data) => {
         const transformedBenchmarks = data
@@ -128,13 +127,32 @@ const App = () => {
               model_name: benchmark.model_name,
               model_size: rawModelSize,
               formatted_model_size: rawModelSize ? rawModelSize.toLocaleString() : "N/A",
-              tokens_per_second: parseFloat(calculateMean(benchmark.tokens_per_second).toFixed(2)),
-              gpu_mem_usage: parseFloat(bytesToGB(calculateMean(benchmark.gpu_mem_usage)).toFixed(2)),
+              tokens_per_second: parseFloat(calculateMean(benchmark.tokens_per_second)),
+              gpu_mem_usage: parseFloat(bytesToGB(calculateMean(benchmark.gpu_mem_usage))),
               quantization_bits: benchmark.quantization_bits && benchmark.quantization_bits !== "unknown" ? benchmark.quantization_bits : "None",
               model_dtype: benchmark.model_dtype
             };
           });
-        setBenchmarks(transformedBenchmarks);
+
+        const dedupedBenchmarks = transformedBenchmarks.reduce((acc, curr) => {
+          const key = `${curr.model_name}-${curr.framework}-${curr.quantization_bits}-${curr.model_dtype}`;
+          if (acc[key]) {
+            acc[key].tokens_per_second = (acc[key].tokens_per_second + curr.tokens_per_second) / 2;
+            acc[key].gpu_mem_usage = (acc[key].gpu_mem_usage + curr.gpu_mem_usage) / 2;
+            acc[key].model_size = (acc[key].model_size + curr.model_size) / 2;
+          } else {
+            acc[key] = curr;
+          }
+          return acc;
+        }, {});
+
+        const dedupedBenchmarksArray = Object.values(dedupedBenchmarks).map(benchmark => ({
+          ...benchmark,
+          tokens_per_second: parseFloat(benchmark.tokens_per_second.toFixed(2)),
+          gpu_mem_usage: parseFloat(benchmark.gpu_mem_usage.toFixed(2)),
+        }));
+
+        setBenchmarks(dedupedBenchmarksArray);
         setLoading(false);
       })
       .catch((err) => {
