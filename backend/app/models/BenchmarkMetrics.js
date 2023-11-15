153480ccd58066efeb62513e660d6fa5cf1a2e09
drose <david@drose.io>
2023-11-15 18:46:50 -0500

    add quant method for backend

--- backend/app/models/BenchmarkMetrics.js
+++ backend/app/models/BenchmarkMetrics.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const BenchmarkMetricsSchema = new mongoose.Schema({
   framework: String,
   model_name: String,
+  quantization_method: String,
   quantization_bits: String,
   tokens_per_second: [Number],
   gpu_mem_usage: [Number],
