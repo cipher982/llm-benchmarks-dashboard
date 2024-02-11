12a6b111a39f01e7f014284076f7551c9c78e407
drose <david@drose.io>
2024-02-11 16:13:22 +0000

    add logging

---
+++ backend/utils/connectToMongoDB.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import logger from "./logger";
+
+interface Env {
+  MONGODB_URI: string;
+}
+
+// Ensure that necessary environment variables are present
+const env: Env = process.env as any;
+
+async function connectToMongoDB(): Promise<void> {
+  try {
+    logger.info(`Verifying MONGODB_URI: ${env.MONGODB_URI}`);
+
+    await mongoose.connect(env.MONGODB_URI);
+
+    logger.info(`MongoDB connected to ${env.MONGODB_URI}`);
+  } catch (err) {
+    logger.error(`MongoDB connection error: ${JSON.stringify(err)}`);
+  }
+}
+
+export default connectToMongoDB;
---
+++ backend/utils/logger.ts
@@ -0,0 +1,20 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    // Add other transports here if needed, like file transport for saving logs to a file
+  ],
+});
+
+export default logger;
