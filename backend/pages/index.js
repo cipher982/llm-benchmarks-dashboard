526bae25136c3b252bb4a50e78a328bd8db87262
drose <david@drose.io>
2024-02-14 14:04:58 +0000

    add favicons

--- backend/pages/index.js
+++ backend/pages/index.js
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 const HomePage = () => {
     return (
         <div>
+            <Head>
+                <title>David Rose's AI Benchmarks</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <h1>David (Cipher982) Rose is an awesome smart engineer working in AI.</h1>
             <p>Check out the API routes:</p>
             <ul>
