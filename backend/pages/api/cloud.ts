a2b0f7799fbdc92896ee5c6eb4d16a7bae443a04
drose <david@drose.io>
2024-02-14 19:50:51 +0000

    fit cors for prod

--- backend/pages/api/cloud.ts
+++ backend/pages/api/cloud.ts
@@ -4,20 +4,16 @@ import { cleanTransformCloud } from '../../utils/processCloud';
 import { handleApiRequest } from '../../utils/apiUtils';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("CORS Debug: Received request", { method: req.method, origin: req.headers.origin }); // Debug log for all requests
+  console.log("CORS Debug: Setting CORS headers");
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.llm-benchmarks.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-  // Enable CORS only in development
-  if (process.env.NODE_ENV !== "production") {
-    console.log("CORS Debug: Setting CORS headers for development environment"); // Debug log for setting headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust accordingly for security
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-    // Handle OPTIONS request without calling the actual API logic
-    if (req.method === 'OPTIONS') {
-      console.log("CORS Debug: Handling OPTIONS request"); // Debug log for OPTIONS request
-      return res.status(200).end();
-    }
+  if (req.method === 'OPTIONS') {
+    console.log("CORS Debug: Handling OPTIONS request");
+    return res.status(200).end();
   }
-  await handleApiRequest(req, res, CloudMetrics, cleanTransformCloud, `cloudMetrics:365days`);
-};
+
+  if (req.method !== 'OPTIONS') {
+    await handleApiRequest(req, res, CloudMetrics, cleanTransformCloud, `cloudMetrics:365days`);
+  }
