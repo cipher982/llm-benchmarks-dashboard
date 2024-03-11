b6d9061802838e4cab272886f82a3fdbd94f713e
drose <david@drose.io>
2024-03-11 21:21:17 +0100

    update model mapping

--- frontend/src/utils/modelMapping.ts
+++ frontend/src/utils/modelMapping.ts
@@ -19,6 +19,7 @@ export const mapModelNames = (data: CloudBenchmark[]): CloudBenchmark[] => {
         "llama2-13b-chat": "llama-2-13b",
         "accounts/fireworks/models/llama-v2-13b": "llama-2-13b",
         "accounts/fireworks/models/llama-v2-13b-chat": "llama-2-13b",
+        "meta.llama2-13b-chat-v1": "llama-2-13b",
 
         // llama 34b - code
         "accounts/fireworks/models/llama-v2-34b-code": "llama-2-34b",
@@ -32,21 +33,29 @@ export const mapModelNames = (data: CloudBenchmark[]): CloudBenchmark[] => {
         "meta-llama/llama-2-70b-chat": "llama-2-70b",
         "accounts/fireworks/models/llama-v2-70b": "llama-2-70b",
         "accounts/fireworks/models/llama-v2-70b-chat": "llama-2-70b",
+        "meta.llama2-70b-chat-v1": "llama-2-70b",
 
         // mistral 7b
         "mistralai/Mistral-7B-Instruct-v0.2": "mistral-7b",
         "mistralai/Mistral-7B-Instruct-v0.1": "mistral-7b",
         "mistralai/mistral-7b-instruct": "mistral-7b",
         "accounts/fireworks/models/mistral-7b": "mistral-7b",
+        "mistral.mistral-7b-instruct-v0:2": "mistral-7b",
 
         // mistral 8x7b
         "mistralai/Mixtral-8x7B-Instruct-v0.1": "mistral-8x7b",
         "mistralai/mixtral-8x7b-instruct": "mistral-8x7b",
         "accounts/fireworks/models/mixtral-8x7b": "mistral-8x7b",
         "accounts/fireworks/models/mixtral-8x7b-instruct": "mistral-8x7b",
+        "mistral.mixtral-8x7b-instruct-v0:1": "mistral-8x7b",
+
+        // mistral large
+        "mistral-large": "mistral-large",
 
         // claude-instant
         "anthropic.claude-instant-v1": "claude-instant-1",
+        "claude-instant-1.2": "claude-instant-1",
+        "claude-instant-1": "claude-instant-1",
 
         // claude-1
         "anthropic.claude-v1": "claude-1",
@@ -55,20 +64,26 @@ export const mapModelNames = (data: CloudBenchmark[]): CloudBenchmark[] => {
         "claude-2": "claude-2",
         "claude-2.1": "claude-2",
         "anthropic.claude-v2": "claude-2",
+        "anthropic.claude-v2:1": "claude-2",
 
-        // claude-instant
-        "claude-instant-1": "claude-instant-1",
-        "claude-instant-1.2": "claude-instant-1",
+        // claude-3 sonnet
+        "claude-3-sonnet-20240229": "claude-3-sonnet",
+        "anthropic.claude-3-sonnet-20240229-v1:0": "claude-3-sonnet",
+
+        // claude-3 opus
+        "claude-3-opus-20240229": "claude-3-opus",
 
         // gpt-3.5
         "gpt-3.5-turbo-0613": "gpt-3.5-turbo",
-
-        // gpt-3.5-16k
-        "gpt-3.5-turbo-16k-0613": "gpt-3.5-turbo-16k",
+        "gpt-3.5-turbo-16k-0613": "gpt-3.5-turbo",
 
         // gpt-4
         "gpt-4-0613": "gpt-4",
         "gpt-4-0314": "gpt-4",
+
+        // gpt-4-turbo
+        "gpt-4-1106-preview": "gpt-4-turbo",
+        "gpt-4-0125-preview": "gpt-4-turbo",
 
         // falcon
         "togethercomputer/falcon-7b": "falcon-7b",
