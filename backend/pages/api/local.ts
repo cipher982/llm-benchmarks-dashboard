409ebb21438aee42fc1fca8b04cce92b7d4eb2e8
drose <david@drose.io>
2024-12-18 19:31:53 -0600

    fix cors issue

--- backend/pages/api/local.ts
+++ backend/pages/api/local.ts
@@ -3,12 +3,17 @@ import { LocalMetrics } from '../../models/BenchmarkMetrics';
 import { cleanTransformLocal } from '../../utils/processLocal';
 import { handleCachedApiResponse } from '../../utils/cacheUtils';
 import { CACHE_KEYS, DEFAULT_RANGES } from '../../utils/cacheUtils';
+import { corsMiddleware } from '../../utils/apiMiddleware';
 
 export const daysAgo = 1000;
 const debug = false;
 const useCache = !debug;
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  // Handle CORS preflight
+  const corsHandled = await corsMiddleware(req, res);
+  if (corsHandled) return;
+
   await handleCachedApiResponse(
     req, 
     res, 
@@ -17,4 +22,4 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     CACHE_KEYS.LOCAL_METRICS,
     DEFAULT_RANGES.LOCAL
   );
-};
+}
