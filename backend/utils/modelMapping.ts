ba02d8581a69a184688f74d8df9b45c863391482
drose <david@drose.io>
2025-01-27 13:40:15 -0600

    fix syntax error

--- backend/utils/modelMapping.ts
+++ backend/utils/modelMapping.ts
@@ -316,8 +316,7 @@ export const mapModelNames = (data: CloudBenchmark[]): CloudBenchmark[] => {
         "deepseek-ai/deepseek-llm-67b-chat": "deepseek-67b",
 
         // deepseek v3
-        "accounts/fireworks/models/deepseek-v3": "deepseek-v3",
-
+        "accounts/fireworks/models/deepseek-v3": "deepseek-v3"
 
     };
 
