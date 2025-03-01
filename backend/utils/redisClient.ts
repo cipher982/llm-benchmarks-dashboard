88f92e1e9a19b1cb7085e3343a1a22d0c2e9b3c4
drose <david.github@drose.io>
2025-03-01 19:02:27 +0100

    ensure redis db is set by env var

--- backend/utils/redisClient.ts
+++ backend/utils/redisClient.ts
@@ -4,6 +4,7 @@ const redisClient = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
+    db: Number(process.env.REDIS_DB),
 });
 
 export default redisClient;
